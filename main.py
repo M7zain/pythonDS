10bdcd91b8543055e8d0602a97878029ced0ec51
M7zain <zeynkara91@gmail.com>
2024-07-29 20:26:00 +0300

    math and math helpers

--- main.py
+++ main.py
@@ -19,4 +19,32 @@ print(5//2)
 
 print(-3 // 2)
 
-#
+#modding is similar to most languages
+print(10 % 3)
+#answer : 1
+
+
+#except for negeative values
+print(-10 % 3)
+#answer : 2
+
+#to be consistent with other languages modulo 
+import math 
+print(math.fmod(-10,3))
+#answer : -1.0
+
+#more math helpers
+print(math.floor(3/2))
+print(math.ceil(3/2))
+print(math.sqrt(2))
+print(math.pow(2,3))
+
+#max / min Int 
+
+float("inf")
+float("-inf")
+
+
+
+
+
