5b415580db674125f22c997eae2c9d52451d2aaa
M7zain <zeynkara91@gmail.com>
2024-07-29 20:20:05 +0300

    division

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 #for(int i=0 ; i<3 ; i++)
 
 for i in range(3): 
@@ -12,3 +10,13 @@ for i in range(2,6):
 #for(int i =5; i>1 ; i--)
 for i in range(5,1, -1): 
     print(i)
+
+#Division is decimal by default 
+print(5/2)
+
+#Double slash rounds down
+print(5//2)
+
+print(-3 // 2)
+
+#
